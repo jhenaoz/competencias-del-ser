54c4cdaef302234ab658adc180cd47260aee09e2
Julián Vanegas Piedrahita <julianvanegas357@gmail.com>
2017-02-13 15:46:02 -0500

    First test commit

---
+++ src/main/java/co/com/psl/evaluacionser/config/MvcConfiguration.java
@@ -0,0 +1,54 @@
+package co.com.psl.evaluacionser.config;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.MimeMappings;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Configuration;
+import javax.servlet.DispatcherType;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.EnumSet;
+
+@Configuration
+public class MvcConfiguration implements ServletContextInitializer, EmbeddedServletContainerCustomizer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        EnumSet<DispatcherType> disps = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.ASYNC);
+    }
+
+    private String resolvePathPrefix() {
+        String fullExecutablePath = this.getClass().getResource("").getPath();
+        String rootPath = Paths.get(".").toUri().normalize().getPath();
+        String extractedPath = fullExecutablePath.replace(rootPath, "");
+        int extractionEndIndex = extractedPath.indexOf("target/");
+        if(extractionEndIndex <= 0) {
+            return "";
+        }
+        return extractedPath.substring(0, extractionEndIndex);
+    }
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        MimeMappings mappings = new MimeMappings(MimeMappings.DEFAULT);
+        // IE issue, see https://github.com/jhipster/generator-jhipster/pull/711
+        mappings.add("html", "text/html;charset=utf-8");
+        // CloudFoundry issue, see https://github.com/cloudfoundry/gorouter/issues/64
+        mappings.add("json", "text/html;charset=utf-8");
+        container.setMimeMappings(mappings);
+        // When running in an IDE or with ./mvnw spring-boot:run, set location of the static web assets.
+        setLocationForStaticAssets(container);
+    }
+
+    private void setLocationForStaticAssets(ConfigurableEmbeddedServletContainer container) {
+        File root;
+        String prefixPath = resolvePathPrefix();
+        root = new File(prefixPath + "src/main/resources/static/app");
+        if (root.exists() && root.isDirectory()) {
+            container.setDocumentRoot(root);
+        }
+    }
+}
---
+++ src/test/java/co/com/psl/evaluacionser/domain/PersonTest.java
@@ -0,0 +1,13 @@
+package co.com.psl.evaluacionser.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PersonTest {
+
+    @Test
+    public void home() throws Exception {
+        Assert.assertEquals(true, true);
+    }
+
+}
