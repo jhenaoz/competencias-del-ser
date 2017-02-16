0e4835794615304ab07ea7e235ffa4015b43b691
Josias Daniel Montoya Campo <jmontoyac@pslcol.com.co>
2017-02-16 13:46:23 -0500

    Added an AptitudeDto for the controller

---
+++ src/main/java/co/com/psl/evaluacionser/persistence/AptitudeTransformer.java
@@ -0,0 +1,12 @@
+package co.com.psl.evaluacionser.persistence;
+
+import co.com.psl.evaluacionser.domain.Aptitude;
+import co.com.psl.evaluacionser.domain.AptitudeDto;
+
+public class AptitudeTransformer {
+
+	public static AptitudeDto convertToDto(Aptitude aptitude) {
+		return new AptitudeDto(aptitude.getId().toString(), aptitude.getEs(), aptitude.getEn());
+	}
+
+}
