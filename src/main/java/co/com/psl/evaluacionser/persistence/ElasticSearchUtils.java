ced860126b78aa1f014ed2f124edcbb127846754
Julián Vanegas Piedrahita <julianvanegas357@gmail.com>
2017-02-16 15:34:12 -0500

    Added STG environment and deleted useless file

--- src/main/java/co/com/psl/evaluacionser/persistence/ElasticSearchUtils.java
+++ src/main/java/co/com/psl/evaluacionser/persistence/ElasticSearchUtils.java
@@ -3,11 +3,20 @@ package co.com.psl.evaluacionser.persistence;
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
 import io.searchbox.client.config.HttpClientConfig;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.net.Inet4Address;
 
 public class ElasticSearchUtils {
 
     // TODO: Parameterize me!
-    private static final String ELASTIC_SEARCH_URL = "http://localhost:9200";
+    @Value("${elasticport}")
+    private static String port;
+
+    @Value("${elastichost}")
+    private static String host;
+
+    private static String ELASTIC_SEARCH_URL = host + ":" + port;
 
     private static JestClientFactory factory = new JestClientFactory();
 
