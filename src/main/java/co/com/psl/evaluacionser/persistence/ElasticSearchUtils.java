e162a724cd57f7a4e328cdd7d9d622bfeec7005b
Julián Vanegas Piedrahita <julianvanegas357@gmail.com>
2017-02-16 15:45:56 -0500

    Fixed ElasticSearchUtils as a Spring Bean

--- src/main/java/co/com/psl/evaluacionser/persistence/ElasticSearchUtils.java
+++ src/main/java/co/com/psl/evaluacionser/persistence/ElasticSearchUtils.java
@@ -4,34 +4,25 @@ import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
 import io.searchbox.client.config.HttpClientConfig;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-import java.net.Inet4Address;
-
+@Configuration
 public class ElasticSearchUtils {
 
-    // TODO: Parameterize me!
     @Value("${elasticport}")
-    private static String port;
+    private String port;
 
     @Value("${elastichost}")
-    private static String host;
+    private String host;
 
-    private static String ELASTIC_SEARCH_URL = host + ":" + port;
+    private String ELASTIC_SEARCH_URL = host + ":" + port;
 
-    private static JestClientFactory factory = new JestClientFactory();
+    private JestClientFactory factory = new JestClientFactory();
 
-    public static synchronized JestClient getClient() {
+    @Bean
+    public JestClient getClient() {
         factory.setHttpClientConfig(new HttpClientConfig.Builder(ELASTIC_SEARCH_URL).multiThreaded(true).build());
         return factory.getObject();
-    }
-
-    public static void closeClient(JestClient client) {
-        if (client != null) {
-            try {
-                client.shutdownClient();
-            } catch (Exception ex) {
-                // TODO: Log me!
-            }
-        }
     }
 }
