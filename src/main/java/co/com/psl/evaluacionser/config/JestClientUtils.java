319c3992ec90a5117e3d8ae3848e63f2bac90054
salveara <santiago.aalvear@gmail.com>
2017-02-16 16:34:04 -0500

    url added by properties

--- src/main/java/co/com/psl/evaluacionser/config/JestClientUtils.java
+++ src/main/java/co/com/psl/evaluacionser/config/JestClientUtils.java
@@ -1,5 +1,6 @@
 package co.com.psl.evaluacionser.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,15 +17,20 @@ import io.searchbox.client.config.HttpClientConfig;
 @Configuration
 public class JestClientUtils {
 
-	// TODO this has to be parametrized in a properties file
-	private static final String ELASTIC_SEARCH_URL = "http://localhost:9200";
+	//Application var for the hot and the port
+	@Value("${elasticport}")
+	private String port;
 
-	//Creates the bean for be consumed in all the app
+	@Value("${elastichost}")
+	private String host;
+
+	// Creates the bean for be consumed in all the app
 	@Bean
 	public JestClient getClient() {
+		String url = "http://" + host + ":" + port;
 		JestClient client;
 		JestClientFactory factory = new JestClientFactory();
-		factory.setHttpClientConfig(new HttpClientConfig.Builder(ELASTIC_SEARCH_URL).multiThreaded(true).build());
+		factory.setHttpClientConfig(new HttpClientConfig.Builder(url).multiThreaded(true).build());
 		client = factory.getObject();
 
 		return client;
