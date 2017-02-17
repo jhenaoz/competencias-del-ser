d3de023d68bd0352d95c75a6c309a6a4ef3d9556
Adrián Jiménez Carrillo <adrianx30@gmail.com>
2017-02-17 08:30:35 -0500

    Added pages and components (#7)
    Added basic front-end setup

--- src/main/resources/static/src/app/app.module.ts
+++ src/main/resources/static/src/app/app.module.ts
@@ -1,27 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule, Http } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
-import {
-  EmployeeService
-} from './employee/index';
+import { EmployeeService } from './employee/index';
+
+import { appRoutes } from './routes';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SurveyOptionsComponent } from './survey-options/survey-options.component';
+import { SurveyComponent } from './survey/survey.component';
+
+//Translate imports
+import { TranslateModule } from 'ng2-translate';
+import { TranslateLoader, TranslateStaticLoader } from "ng2-translate/src/translate.service";
+//CSS & Javascript imports
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EmployeeComponent
-  ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     FormsModule,
-    HttpModule
+    ReactiveFormsModule,   
+    HttpModule,
+    TranslateModule.forRoot({
+            provide: TranslateLoader,
+            useFactory: translateLoaderFactory,
+            deps: [Http]
+        })
   ],
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    NavbarComponent,
+    FooterComponent,
+    WelcomeComponent,
+    SurveyOptionsComponent,
+    SurveyComponent
+  ],  
   providers: [
-    EmployeeService
+    EmployeeService,
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export function translateLoaderFactory(http: any) {
+    return new TranslateStaticLoader(http, 'assets/i18n', '.json');
+}
---
+++ src/main/resources/static/src/app/survey-options/survey-options.component.spec.ts
@@ -0,0 +1,26 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { SurveyOptionsComponent } from './survey-options.component';
+
+describe('SurveyOptionsComponent', () => {
+  let component: SurveyOptionsComponent;
+  let fixture: ComponentFixture<SurveyOptionsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SurveyOptionsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SurveyOptionsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+
+});
