df27075f556562acdacc1032caf73c70e604fc85
jmontoyac0 <jmontoyac@psl.com.co>
2017-02-17 08:49:12 -0500

    Update JestClientUtils.java

--- src/main/java/co/com/psl/evaluacionser/config/JestClientUtils.java
+++ src/main/java/co/com/psl/evaluacionser/config/JestClientUtils.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 
 public class JestClientUtils {
 
-    //Application var for the hot and the port
+    //Application var for the host and the port
     @Value("${elasticport}")
     private String port;
 
