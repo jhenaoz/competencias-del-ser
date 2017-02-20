444e424cb439dad0d2fcb0f51466fae06ba7d317
fcastillao <fcastillao@psl.com.co>
2017-02-20 07:31:53 -0500

    returned Behavior ID to String

--- src/test/java/co/com/psl/evaluacionser/controller/AptitudeDtoTransformerTest.java
+++ src/test/java/co/com/psl/evaluacionser/controller/AptitudeDtoTransformerTest.java
@@ -14,8 +14,8 @@ public class AptitudeDtoTransformerTest {
 
 	@Test
 	public void aptitudeTransformedToDto() {
-		Behavior behavior1 = new Behavior(1L, "Reconoce sus errores", "Recognize his mistakes");
-		Behavior behavior2 = new Behavior(2L, "Solicita sugerencias", "Asks for suggestions");
+		Behavior behavior1 = new Behavior("1", "Reconoce sus errores", "Recognize his mistakes");
+		Behavior behavior2 = new Behavior("2", "Solicita sugerencias", "Asks for suggestions");
 		List<Behavior> behaviors = new ArrayList<Behavior>();
 		behaviors.add(behavior1);
 		behaviors.add(behavior2);
