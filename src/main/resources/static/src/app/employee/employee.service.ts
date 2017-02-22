928251e50e47ee807affa286d5255ffb8ed1e009
Adrian Jimenez <adrianx30@gmail.com>
2017-02-22 11:47:05 -0500

    Service url

--- src/main/resources/static/src/app/employee/employee.service.ts
+++ src/main/resources/static/src/app/employee/employee.service.ts
@@ -11,7 +11,7 @@ import { IEmployee } from './employee.model';
 
 @Injectable()
 export class EmployeeService {
-  private _employeeUrl = 'https://anypoint.mulesoft.com/apiplatform/proxy/https://mocksvc.mulesoft.com/mocks/d4804468-6192-482e-a2eb-53dca0d66495/person';
+  private _employeeUrl = 'https://localhost:8080/person' //'https://anypoint.mulesoft.com/apiplatform/proxy/https://mocksvc.mulesoft.com/mocks/d4804468-6192-482e-a2eb-53dca0d66495/person';
 
   constructor(private _http: Http) { }
 
