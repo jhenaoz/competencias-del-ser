82588f1f603104df9df2961bc9b975e4ab1ca613
salveara <santiago.aalvear@gmail.com>
2017-02-23 08:37:45 -0500

    Constructor, getter and setter survey class

--- src/main/java/co/com/psl/evaluacionser/domain/Survey.java
+++ src/main/java/co/com/psl/evaluacionser/domain/Survey.java
@@ -15,5 +15,68 @@ public class Survey {
     private String date;
     
     List<AptitudeSurvey> aptitudeSurvey;
+
+    public Survey() {
+        super();
+    }
+    
+    public Survey(String id, String evaluated, String evaluator, String rol, String date,
+            List<AptitudeSurvey> aptitudeSurvey) {
+        super();
+        this.id = id;
+        this.evaluated = evaluated;
+        this.evaluator = evaluator;
+        this.rol = rol;
+        this.date = date;
+        this.aptitudeSurvey = aptitudeSurvey;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEvaluated() {
+        return evaluated;
+    }
+
+    public void setEvaluated(String evaluated) {
+        this.evaluated = evaluated;
+    }
+
+    public String getEvaluator() {
+        return evaluator;
+    }
+
+    public void setEvaluator(String evaluator) {
+        this.evaluator = evaluator;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public List<AptitudeSurvey> getAptitudeSurvey() {
+        return aptitudeSurvey;
+    }
+
+    public void setAptitudeSurvey(List<AptitudeSurvey> aptitudeSurvey) {
+        this.aptitudeSurvey = aptitudeSurvey;
+    }
     
 }
