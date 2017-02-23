ec9438fb812137070ac85f97cf68c3a690ae2824
fcastillao <fcastillao@psl.com.co>
2017-02-23 16:53:15 -0500

    removed inner classes
    now AptitudeSurveyDto and BehaviorSurveyDto are separate classes

---
+++ src/main/java/co/com/psl/evaluacionser/domain/AptitudeSurveyDto.java
@@ -0,0 +1,43 @@
+package co.com.psl.evaluacionser.domain;
+
+import java.util.List;
+
+class AptitudeSurveyDto {
+
+    private String aptitudeId;
+    private String observation;
+    private List<BehaviorSurveyDto> behaviors;
+
+    public AptitudeSurveyDto() {
+    }
+
+    public AptitudeSurveyDto(String aptitudeId, String observation, List<BehaviorSurveyDto> behaviors) {
+        this.aptitudeId = aptitudeId;
+        this.observation = observation;
+        this.behaviors = behaviors;
+    }
+
+    public String getAptitudeId() {
+        return aptitudeId;
+    }
+
+    public void setAptitudeId(String aptitudeId) {
+        this.aptitudeId = aptitudeId;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+
+    public List<BehaviorSurveyDto> getBehaviors() {
+        return behaviors;
+    }
+
+    public void setBehaviors(List<BehaviorSurveyDto> behaviors) {
+        this.behaviors = behaviors;
+    }
+}
