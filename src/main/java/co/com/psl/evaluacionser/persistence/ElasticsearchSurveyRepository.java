311f1feac2e65b7e591e703dbfd71ede9a85fe49
fcastillao <fcastillao@psl.com.co>
2017-02-24 10:19:02 -0500

    added survey saving

--- src/main/java/co/com/psl/evaluacionser/persistence/ElasticsearchSurveyRepository.java
+++ src/main/java/co/com/psl/evaluacionser/persistence/ElasticsearchSurveyRepository.java
@@ -1,14 +1,31 @@
 package co.com.psl.evaluacionser.persistence;
 
 import co.com.psl.evaluacionser.domain.Survey;
+import io.searchbox.client.JestClient;
+import io.searchbox.core.Index;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
-public class ElasticsearchSurveyRepository implements SurveyRepository{
+import java.io.IOException;
 
-    //TODO implementar guardado de Survey
+@Component
+public class ElasticsearchSurveyRepository implements SurveyRepository {
+
+    private static final String APTITUDE_INDEX_NAME = "survey";
+    private static final String APTITUDE_TYPE_NAME = "survey";
+
+    @Autowired
+    private JestClient client;
+
     @Override
     public Survey saveSurvey(Survey survey) {
-        return null;
+        Index index = new Index.Builder(survey).index(APTITUDE_INDEX_NAME).type(APTITUDE_TYPE_NAME).build();
+        try {
+            client.execute(index);
+            return survey;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 }
