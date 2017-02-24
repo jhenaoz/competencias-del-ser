42f8bba2a28929fdcf9ea188a8814f986b14a88f
salveara <santiago.aalvear@gmail.com>
2017-02-24 09:09:51 -0500

    test fail fixed

--- src/test/java/co/com/psl/evaluacionser/domain/BehaviorSurveyTest.java
+++ src/test/java/co/com/psl/evaluacionser/domain/BehaviorSurveyTest.java
@@ -11,14 +11,13 @@ public class BehaviorSurveyTest {
         Behavior behavior = new Behavior("1", "Accepts review", "Acepta retroalimentacion");
         BehaviorSurvey behaviourSurvey = new BehaviorSurvey(behavior, 2);
         assertEquals("1", behaviourSurvey.getBehavior().getId());
-        assertEquals("2", behaviourSurvey.getScore());
+        assertEquals(2, behaviourSurvey.getScore());
     }
 
     @Test
     public void EmptyConstructorShouldBeNull() {
         BehaviorSurvey behaviourSurvey = new BehaviorSurvey();
         assertNull(behaviourSurvey.getBehavior());
-        assertNull(behaviourSurvey.getScore());
     }
 
     @Test
@@ -28,7 +27,7 @@ public class BehaviorSurveyTest {
         behaviourSurvey.setBehavior(behavior);
         behaviourSurvey.setScore(2);
         assertEquals("1", behaviourSurvey.getBehavior().getId());
-        assertEquals("2", behaviourSurvey.getScore());
+        assertEquals(2, behaviourSurvey.getScore());
     }
 
 }
