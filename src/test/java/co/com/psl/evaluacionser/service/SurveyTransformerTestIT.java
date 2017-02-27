8864bd3e15f75b8532bf2f94bcfdaf42212c65df
salveara <santiago.aalvear@gmail.com>
2017-02-27 13:01:49 -0500

    Test complete

--- src/test/java/co/com/psl/evaluacionser/service/SurveyTransformerTestIT.java
+++ src/test/java/co/com/psl/evaluacionser/service/SurveyTransformerTestIT.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import co.com.psl.evaluacionser.domain.Survey;
+import co.com.psl.evaluacionser.service.dto.SurveyDto;
 import co.com.psl.evaluacionser.service.transformer.SurveyTransformer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -59,4 +61,19 @@ public class SurveyTransformerTestIT {
         assertEquals("Apertura", aptitudeSurvey.getAptitude().getEs());
     }
 
+    @Test
+    public void SurveyTransformerTest() {
+        List<BehaviorSurveyDto> behaviorsSurveyDto = new ArrayList<>();
+        behaviorsSurveyDto.add(new BehaviorSurveyDto("1", 5));
+        behaviorsSurveyDto.add(new BehaviorSurveyDto("2", 4));
+        behaviorsSurveyDto.add(new BehaviorSurveyDto("3", 1));
+        AptitudeSurveyDto aptitudeSurveyDto = new AptitudeSurveyDto("1", "Siempre abierto a cambios", behaviorsSurveyDto);
+        List<AptitudeSurveyDto> aptitudeSurveyDtos = new ArrayList<AptitudeSurveyDto>();
+        aptitudeSurveyDtos.add(aptitudeSurveyDto);
+        SurveyDto surveyDto = new SurveyDto("Juan evaluator", "Juana evaluated", "Team", aptitudeSurveyDtos);
+        Survey survey = surveyTransformer.Transformer(surveyDto);
+        assertEquals("Juan evaluator", survey.getEvaluator());
+        assertEquals("Siempre abierto a cambios", survey.getAptitudes().get(0).getObservation());
+        assertEquals(5, survey.getAptitudes().get(0).getBehaviors().get(0).getScore());
+    }
 }
