3936db4b34149cd2f3677ce8a0ab070d939cf084
jmontoyac0 <jmontoyac@psl.com.co>
2017-02-27 16:04:35 -0500

    Update SurveyController.java
    Fixed imports

--- src/main/java/co/com/psl/evaluacionser/controller/SurveyController.java
+++ src/main/java/co/com/psl/evaluacionser/controller/SurveyController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
