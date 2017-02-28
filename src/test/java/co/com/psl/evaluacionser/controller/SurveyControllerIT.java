fd855f2fbdc314554077b709965a2ac0ecda1da6
salveara <santiago.aalvear@gmail.com>
2017-02-28 13:21:40 -0500

    Test surveyController conflicts

--- src/test/java/co/com/psl/evaluacionser/controller/SurveyControllerIT.java
+++ src/test/java/co/com/psl/evaluacionser/controller/SurveyControllerIT.java
@@ -1,5 +1,7 @@
 package co.com.psl.evaluacionser.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +9,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,6 +21,6 @@ public class SurveyControllerIT {
     @Test
     public void emptyBodyRequest() throws Exception {
         mockMvc.perform(get("/survey"))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isMethodNotAllowed());
     }
 }
