23ffcd733b3e4bd2f43ecba782fcceb648ffa026
fcastillao <fcastillao@psl.com.co>
2017-03-02 14:24:48 -0500

    integration tests for person

--- src/main/java/co/com/psl/evaluacionser/persistence/ElasticsearchPersonRepository.java
+++ src/main/java/co/com/psl/evaluacionser/persistence/ElasticsearchPersonRepository.java
@@ -6,12 +6,13 @@ import io.searchbox.core.Index;
 import io.searchbox.core.Search;
 import io.searchbox.core.SearchResult;
 import io.searchbox.core.SearchResult.Hit;
+import org.apache.log4j.Logger;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.util.List;
@@ -23,22 +24,19 @@ import java.util.stream.Collectors;
 @Component
 public class ElasticsearchPersonRepository implements PersonRepository {
 
+    static Logger logger = Logger.getLogger(ElasticsearchPersonRepository.class);
     /**
      * These Strings must be congruent with the elasticsearch database
      */
     @Value("${elasticPersonIndex}")
     private String personIndexName;
-
     @Value("${elasticPersonType}")
     private String personTypeName;
-
     /**
      * Calls the JestClient defined as a bean
      */
     @Autowired
     private JestClient client;
-
-    static Logger logger = Logger.getLogger(ElasticsearchPersonRepository.class);
 
     /**
      * This method find all indexes person with type employee
@@ -54,6 +52,9 @@ public class ElasticsearchPersonRepository implements PersonRepository {
                 .addType(personTypeName).build();
         try {
             result = client.execute(search);
+            if (!result.isSucceeded())
+                return null;
+
         } catch (IOException e) {
             logger.error("The search can't be completed " + e.getMessage());
         }
@@ -67,13 +68,55 @@ public class ElasticsearchPersonRepository implements PersonRepository {
 
     @Override
     public Person save(Person person) {
-        Index index = new Index.Builder(person).index(personIndexName).type(personTypeName).build();
+        Index index = new Index.Builder(person).index(personIndexName).type(personTypeName).refresh(true).build();
         try {
             client.execute(index);
             return person;
         } catch (IOException e) {
             logger.error("The person can't be saved " + e.getMessage());
             throw new RuntimeException(e);
+        }
+    }
+
+    public Person findPersonById(String personId) {
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchQuery("id", personId));
+
+        Search search = new Search.Builder(searchSourceBuilder.toString()).addIndex(personIndexName).build();
+
+        try {
+            SearchResult result = client.execute(search);
+
+            if (!result.isSucceeded())
+                return null;
+
+            return getPerson(result.getFirstHit(Person.class));
+        } catch (IOException e) {
+            logger.error("The person with the given id couldn't be found " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public boolean deletePersonByIdAndName(String id, String name) {
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery()
+                .must(QueryBuilders.termQuery("id", id))
+                .must(QueryBuilders.matchPhraseQuery("name", name));
+
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(boolQueryBuilder);
+
+        DeleteByQueryOW deleteSpecificPerson = new DeleteByQueryOW.Builder(searchSourceBuilder.toString())
+                .addIndex(personIndexName)
+                .refresh(true)
+                .build();
+
+        try {
+            client.execute(deleteSpecificPerson);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
         }
     }
 }
