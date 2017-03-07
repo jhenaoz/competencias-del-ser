10a757f976fd164dde721803ee10503b21bf2f84
Julián Vanegas Piedrahita <julianvanegas357@gmail.com>
2017-03-07 08:54:47 -0500

    Remove SupressWarnings from Application.java

--- src/main/java/co/com/psl/evaluacionser/Application.java
+++ src/main/java/co/com/psl/evaluacionser/Application.java
@@ -3,7 +3,6 @@ package co.com.psl.evaluacionser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SuppressWarnings("hideutilityclassconstructor")
 @SpringBootApplication
 public class Application {
 
