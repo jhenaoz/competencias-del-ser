6c064ed3fa7f3958b4d3ec7e1824a5c9b2dd0de3
salveara <santiago.aalvear@gmail.com>
2017-03-07 10:08:24 -0500

    HTML template

--- src/main/java/co/com/psl/evaluacionser/service/MailService.java
+++ src/main/java/co/com/psl/evaluacionser/service/MailService.java
@@ -29,6 +29,7 @@ public class MailService {
 
     /**
      * This Method create a new mail when a new survey is done
+     * To send a Email it's necessary to allow the gmail account to be acceded by a not secure application
      * @param survey to extract the necessary information to make the new email
      */
     public void newSurveyMail(Survey survey) throws AddressException, MessagingException {
@@ -53,11 +54,11 @@ public class MailService {
         generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(mailReceiver));
         generateMailMessage.addRecipient(Message.RecipientType.CC, new InternetAddress(mailUsername));
         generateMailMessage.setSubject("Nueva valoracion del ser");
-        String emailBody = constructMessageText(survey);
+        String emailBody = constructHTML(survey);
         /**
          * generateMailMessage.setContent(emailBody, "text/html"); can send html text in this way
          */
-        generateMailMessage.setContent(emailBody, "text/plain");
+        generateMailMessage.setContent(emailBody, "text/html; charset=utf-16");
 
         /**
          * Get Session and Send mail
@@ -70,17 +71,67 @@ public class MailService {
     }
 
     /**
-     * This method create the text for the new email with the survey information
+     * This method create the html text for the new email with the survey information
      *
      * @param survey the new survey
      * @return the new message
      */
-    public String constructMessageText(Survey survey) {
-        return "Cordial saludo, se acaba de realizar una valoración del ser \n"
-                + "Persona valorada: " + survey.getEvaluated() + "\n"
-                + "Persona que realizó la valoración: " + survey.getEvaluator() + "\n"
-                + "Tipo de valoracón: "
-                + evaluationType(survey.getAptitudes());
+    public String constructHTML(Survey survey) {
+        return "<html>\n" +
+                "<head>\n" +
+                "<style>\n" +
+                "* {\n" +
+                "    font-family: Arial, Helvetica, sans-serif;\n" +
+                "}\n" +
+                "h2, h4{\n" +
+                "    color: #134C96;\n" +
+                "    font-weight: bold;\n" +
+                "}\n" +
+                "html {\n" +
+                "  height: 100%;\n" +
+                "  box-sizing: border-box;\n" +
+                "}\n" +
+                "*,\n" +
+                "*:before,\n" +
+                "*:after {\n" +
+                "  box-sizing: inherit;\n" +
+                "}\n" +
+                "body {\n" +
+                "  position: relative;\n" +
+                "  margin: 0;\n" +
+                "  padding-bottom: 6rem;\n" +
+                "  min-height: 100%;\n" +
+                "}\n" +
+                ".box {\n" +
+                "  margin: 0 auto;\n" +
+                "  padding-top: 10%;\n" +
+                "  max-width: 640px;\n" +
+                "  width: 94%;\n" +
+                "}\n" +
+                ".box h1 {\n" +
+                "  margin-top: 0;\n" +
+                "}\n" +
+                ".footer {\n" +
+                "  position: absolute;\n" +
+                "  right: 0;\n" +
+                "  bottom: 0;\n" +
+                "  left: 0;\n" +
+                "  padding: 1rem;\n" +
+                "  background-color: #efefef;\n" +
+                "  text-align: center;\n" +
+                "}\n" +
+                "</style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<div class=\"footer\"></div>\n" +
+                "<div class=\"box\">\n" +
+                "<h2>Cordial saludo, se ha realizado una valoración del ser</h2> \n" +
+                " <h4>Persona valorada:</h4> \n" +
+                " <p> " + survey.getEvaluated() + " </p> \n" +
+                " <h4>Persona que realizó la valoración:</h4>\n" +
+                " <p> " + survey.getEvaluator() + " </p> \n" +
+                " <h4>Tipo de valoracón:</h4> \n" +
+                evaluationType(survey.getAptitudes());
     }
 
     /**
@@ -90,11 +141,20 @@ public class MailService {
      * @return the part of the message with the type
      */
     public String evaluationType(List<AptitudeSurvey> aptitudeSurveyList) {
+        String type;
         if (aptitudeSurveyList.size() == 1) {
-            return "una competencia de una persona \n" + "Competencia evaluada: "
-                    + aptitudeSurveyList.get(0).getAptitude().getEs();
+            type = " <p> Una competencia de una persona</p> \n" +
+                    " <h4>Competencia evaluada:</h4> \n" +
+                    " <p> " + aptitudeSurveyList.get(0).getAptitude().getEs() + "</p>\n";
+
+
         } else {
-            return "Todas las competencias de una persona";
+            type = " <p>Todas las competencias de una persona</p> \n";
         }
+        type += "</div>\n" +
+                "<div class=\"footer\">This message is auto-generated - <strong>Competencias del ser PSL </strong> - </div>\n" +
+                "</body>\n" +
+                "</html>\n";
+        return type;
     }
 }
--- src/test/java/co/com/psl/evaluacionser/service/MailServiceTest.java
+++ src/test/java/co/com/psl/evaluacionser/service/MailServiceTest.java
@@ -22,19 +22,74 @@ public class MailServiceTest {
                 "Siempre acepta comentarios", behaviorSurveyList);
         List<AptitudeSurvey> aptitudeSurveyList = new ArrayList<>();
         aptitudeSurveyList.add(aptitudeSurvey);
-        survey = new Survey("Juan Evaluator", "Juana Evaluated", "Team",
+        survey = new Survey("Jhon Doe", "Jane Doe", "Team",
                 "2017-06-06", aptitudeSurveyList);
     }
 
     @Test
     public void constructMessageTextWithOneAptitude() {
-        String result = mailService.constructMessageText(survey);
-        String expected = "Cordial saludo, se acaba de realizar una valoración del ser \n"
-                + "Persona valorada: " + "Juana Evaluated" + "\n"
-                + "Persona que realizó la valoración: " + "Juan Evaluator" + "\n"
-                + "Tipo de valoracón: "
-                + "una competencia de una persona \n" + "Competencia evaluada: "
-                + "Apertura";
+        String result = mailService.constructHTML(survey);
+        String expected  = "<html>\n" +
+                "<head>\n" +
+                "<style>\n" +
+                "* {\n" +
+                "    font-family: Arial, Helvetica, sans-serif;\n" +
+                "}\n" +
+                "h2, h4{\n" +
+                "    color: #134C96;\n" +
+                "    font-weight: bold;\n" +
+                "}\n" +
+                "html {\n" +
+                "  height: 100%;\n" +
+                "  box-sizing: border-box;\n" +
+                "}\n" +
+                "*,\n" +
+                "*:before,\n" +
+                "*:after {\n" +
+                "  box-sizing: inherit;\n" +
+                "}\n" +
+                "body {\n" +
+                "  position: relative;\n" +
+                "  margin: 0;\n" +
+                "  padding-bottom: 6rem;\n" +
+                "  min-height: 100%;\n" +
+                "}\n" +
+                ".box {\n" +
+                "  margin: 0 auto;\n" +
+                "  padding-top: 10%;\n" +
+                "  max-width: 640px;\n" +
+                "  width: 94%;\n" +
+                "}\n" +
+                ".box h1 {\n" +
+                "  margin-top: 0;\n" +
+                "}\n" +
+                ".footer {\n" +
+                "  position: absolute;\n" +
+                "  right: 0;\n" +
+                "  bottom: 0;\n" +
+                "  left: 0;\n" +
+                "  padding: 1rem;\n" +
+                "  background-color: #efefef;\n" +
+                "  text-align: center;\n" +
+                "}\n" +
+                "</style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<div class=\"footer\"></div>\n" +
+                "<div class=\"box\">\n" +
+                "<h2>Cordial saludo, se ha realizado una valoración del ser</h2> \n" +
+                " <h4>Persona valorada:</h4> \n" +
+                " <p> Jane Doe </p> \n" +
+                " <h4>Persona que realizó la valoración:</h4>\n" +
+                " <p> Jhon Doe </p> \n" +
+                " <h4>Tipo de valoracón:</h4> \n" +
+                " <p> Una competencia de una persona</p> \n" +
+                " <h4>Competencia evaluada:</h4> \n" +
+                " <p> Apertura</p>\n" +
+                "</div>\n" +
+                "<div class=\"footer\">This message is auto-generated - <strong>Competencias del ser PSL </strong> - </div>\n" +
+                "</body>\n" +
+                "</html>\n";
         assertEquals(expected, result);
     }
 
@@ -44,12 +99,66 @@ public class MailServiceTest {
         List<AptitudeSurvey> aptitudeSurveyList = survey.getAptitudes();
         aptitudeSurveyList.add(null);
         newSurvey.setAptitudes(aptitudeSurveyList);
-        String result = mailService.constructMessageText(newSurvey);
-        String expected = "Cordial saludo, se acaba de realizar una valoración del ser \n"
-                + "Persona valorada: " + "Juana Evaluated" + "\n"
-                + "Persona que realizó la valoración: " + "Juan Evaluator" + "\n"
-                + "Tipo de valoracón: "
-                + "Todas las competencias de una persona";
+        String result = mailService.constructHTML(newSurvey);
+        String expected = "<html>\n" +
+                "<head>\n" +
+                "<style>\n" +
+                "* {\n" +
+                "    font-family: Arial, Helvetica, sans-serif;\n" +
+                "}\n" +
+                "h2, h4{\n" +
+                "    color: #134C96;\n" +
+                "    font-weight: bold;\n" +
+                "}\n" +
+                "html {\n" +
+                "  height: 100%;\n" +
+                "  box-sizing: border-box;\n" +
+                "}\n" +
+                "*,\n" +
+                "*:before,\n" +
+                "*:after {\n" +
+                "  box-sizing: inherit;\n" +
+                "}\n" +
+                "body {\n" +
+                "  position: relative;\n" +
+                "  margin: 0;\n" +
+                "  padding-bottom: 6rem;\n" +
+                "  min-height: 100%;\n" +
+                "}\n" +
+                ".box {\n" +
+                "  margin: 0 auto;\n" +
+                "  padding-top: 10%;\n" +
+                "  max-width: 640px;\n" +
+                "  width: 94%;\n" +
+                "}\n" +
+                ".box h1 {\n" +
+                "  margin-top: 0;\n" +
+                "}\n" +
+                ".footer {\n" +
+                "  position: absolute;\n" +
+                "  right: 0;\n" +
+                "  bottom: 0;\n" +
+                "  left: 0;\n" +
+                "  padding: 1rem;\n" +
+                "  background-color: #efefef;\n" +
+                "  text-align: center;\n" +
+                "}\n" +
+                "</style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<div class=\"footer\"></div>\n" +
+                "<div class=\"box\">\n" +
+                "<h2>Cordial saludo, se ha realizado una valoración del ser</h2> \n" +
+                " <h4>Persona valorada:</h4> \n" +
+                " <p> Jane Doe </p> \n" +
+                " <h4>Persona que realizó la valoración:</h4>\n" +
+                " <p> Jhon Doe </p> \n" +
+                " <h4>Tipo de valoracón:</h4> \n" +
+                " <p>Todas las competencias de una persona</p> \n" +
+                "</div>\n" +
+                "<div class=\"footer\">This message is auto-generated - <strong>Competencias del ser PSL </strong> - </div>\n" +
+                "</body>\n" +
+                "</html>\n";
         assertEquals(expected, result);
     }
 }
