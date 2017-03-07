a6cc5ed58c18afefd4d915de35a93d475375f4a1
salveara <santia_60@hotmail.com>
2017-03-06 21:02:24 -0500

    Send message complete

--- src/main/java/co/com/psl/evaluacionser/service/MailService.java
+++ src/main/java/co/com/psl/evaluacionser/service/MailService.java
@@ -2,13 +2,10 @@ package co.com.psl.evaluacionser.service;
 
 import co.com.psl.evaluacionser.domain.AptitudeSurvey;
 import co.com.psl.evaluacionser.domain.Survey;
-import org.apache.log4j.Logger;
-import org.springframework.mail.MailException;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import javax.mail.MessagingException;
+import javax.mail.*;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.List;
@@ -21,84 +18,50 @@ import java.util.Properties;
 public class MailService {
 
     //TODO define the address who receive the mail to arodas@psl.com.co in the application properties files
-    //@Value("${mailReceiver}")
-    private String mailReceiver = "lusago19@gmail.com";
+    @Value("${mailReceiver}")
+    private String mailReceiver;
 
-    //@Value("${mailUsername}")
-    private String mailUsername = "competencias.del.ser@gmail.com";
+    @Value("${mailUsername}")
+    private String mailUsername;
 
-    //@Value("${mailPassword}")
-    private String pass = "competenciasdelser";
-
-    static Logger logger = Logger.getLogger(MailService.class);
-
-    /*public void newSurveyMail(SurveyDto surveyDto) {
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.auth", "true");
-
-        Session session = Session.getDefaultInstance(props,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication("username", "pass");
-                    }
-                });
-
-        try {
-
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("from@no-spam.com"));
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse("to@no-spam.com"));
-            message.setSubject("Testing Subject");
-            message.setText("Dear Mail Crawler," +
-                    "\n No spam to my email, please!");
-
-            Transport.send(message);
-
-            System.out.println("Done");
-
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
-    }*/
+    @Value("${mailPassword}")
+    private String mailPassword;
 
     /**
      * This Method create a new mail when a new survey is done
      * @param survey to extract the necessary information to make the new email
      */
-    public void newSurveyMail(Survey survey) {
-        JavaMailSenderImpl sender = new JavaMailSenderImpl();
-        sender.setHost("smtp.gmail.com");
-        sender.setPort(465);
-        sender.setUsername(mailUsername);
-        sender.setPassword(pass);
-        Properties properties = new Properties();
-        properties.setProperty("mail.smtp.auth","true");
-        properties.setProperty("mail.smtp.starttls.enable","true");
-        sender.setJavaMailProperties(properties);
-        MimeMessage message = sender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
-        try {
-            InternetAddress internetEmailReceiver = new InternetAddress();
-            internetEmailReceiver.setAddress(mailReceiver);
-            helper.setTo(internetEmailReceiver);
-            helper.setSubject("Nueva calificación del ser");
-            helper.setText(constructMessageText(survey));
-            sender.send(message);
-        } catch (MailException e) {
-            logger.error("Mail can't be sent " + e);
-        } catch (MessagingException me) {
-            logger.error("Message can't be built " + me);
-        }
+    public void newSurveyMail(Survey survey) throws AddressException, MessagingException {
+
+        Properties mailServerProperties;
+        Session getMailSession;
+        MimeMessage generateMailMessage;
+
+        mailServerProperties = System.getProperties();
+        mailServerProperties.put("mail.smtp.port", "587");
+        mailServerProperties.put("mail.smtp.auth", "true");
+        mailServerProperties.put("mail.smtp.starttls.enable", "true");
+
+        getMailSession = Session.getDefaultInstance(mailServerProperties, null);
+        generateMailMessage = new MimeMessage(getMailSession);
+        generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(mailReceiver));
+        generateMailMessage.addRecipient(Message.RecipientType.CC, new InternetAddress(mailUsername));
+        generateMailMessage.setSubject("Nueva valoracion del ser");
+        String emailBody = constructMessageText(survey);
+        //generateMailMessage.setContent(emailBody, "text/html");
+        generateMailMessage.setContent(emailBody, "text/plain");
+
+        Transport transport = getMailSession.getTransport("smtp");
+
+        transport.connect("smtp.gmail.com", mailUsername, mailPassword);
+        transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
+        transport.close();
     }
 
     /**
      * This method create the text for the new email with the survey information
-     * @param survey
+     *
+     * @param survey the new survey
      * @return the new message
      */
     public String constructMessageText(Survey survey) {
@@ -111,7 +74,8 @@ public class MailService {
 
     /**
      * This method control the type of the survey, one aptitude or all of them
-     * @param aptitudeSurveyList
+     *
+     * @param aptitudeSurveyList the List of aptitudes from survey, to get the size and name
      * @return the part of the message with the type
      */
     public String evaluationType(List<AptitudeSurvey> aptitudeSurveyList) {
