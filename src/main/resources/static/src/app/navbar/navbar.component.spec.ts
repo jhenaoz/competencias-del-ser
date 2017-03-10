91eb07d27f78b9b8cadb65ec39fba08b9f233e15
Adrian Jimenez <adrianx30@gmail.com>
2017-03-10 10:20:42 -0500

    Navbar Tested (Change Language)

--- src/main/resources/static/src/app/navbar/navbar.component.spec.ts
+++ src/main/resources/static/src/app/navbar/navbar.component.spec.ts
@@ -1,17 +1,32 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+
+import { Http } from '@angular/http';
+
+import { TranslateService, TranslateModule } from 'ng2-translate';
+import { TranslateLoader, TranslateStaticLoader } from 'ng2-translate/src/translate.service';
+
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { NavbarComponent } from './navbar.component';
 
-describe('NavbarComponent', () => {
+describe('Component: NavbarComponent', () => {
   let component: NavbarComponent;
+  let service: TranslateService;
   let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+      imports: [
+        ReactiveFormsModule,
+        TranslateModule.forRoot({
+            provide: TranslateLoader,
+            useFactory: translateLoaderFactory,
+            deps: [Http]
+        })
+      ],
+      declarations: [ NavbarComponent ],
     })
     .compileComponents();
   }));
@@ -19,8 +34,32 @@ describe('NavbarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    service = TestBed.get(TranslateService);
   });
 
+  it('should have a defined component', () => {
+        expect(component).toBeDefined();
+  });
+
+  describe('Method: changeLang', () => {
+    it('should change language to EN', () => {
+      spyOn(service, 'use').and.returnValue({});
+      component.changeLang('en');
+      fixture.detectChanges();
+      expect(service.use).toHaveBeenCalledWith('en');
+    });
+
+    it('should change language to ES', () => {
+      spyOn(service, 'use').and.returnValue({});
+      component.changeLang('es');
+      fixture.detectChanges();
+      expect(service.use).toHaveBeenCalledWith('es');
+    });
+
+  });
 
 });
+
+function translateLoaderFactory(http: any) {
+    return new TranslateStaticLoader(http, '', '');
+}
