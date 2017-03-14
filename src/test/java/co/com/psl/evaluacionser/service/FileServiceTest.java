81a8c24d8c3e3686c3d271971a3f503d63101f64
fcastillao <fcastillao@psl.com.co>
2017-03-14 10:58:19 -0500

    checkstyle

--- src/test/java/co/com/psl/evaluacionser/service/FileServiceTest.java
+++ src/test/java/co/com/psl/evaluacionser/service/FileServiceTest.java
@@ -2,31 +2,34 @@ package co.com.psl.evaluacionser.service;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class FileServiceTest {
 
-    FileService fileService = new FileService();
+    private FileService fileService = new FileService();
 
     @Test
     public void getFileNameWithBothDates() throws Exception {
-        StringBuilder fileName = fileService.getFileName("Josias Montoya", "1993-07-05", "2017-03-14");
+        StringBuilder fileName = fileService.getFileName("Josias Montoya",
+                "1993-07-05", "2017-03-14");
 
-        assertEquals("Valoración Competencias Ser Josias Montoya del 1993-07-05 al 2017-03-14.xlsx",fileName.toString());
+        assertEquals("Valoración Competencias Ser Josias Montoya del 1993-07-05 al 2017-03-14.xlsx",
+                fileName.toString());
     }
 
     @Test
     public void getFileNameWithoutDates() throws Exception {
         StringBuilder fileName = fileService.getFileName("Josias Montoya", null, null);
 
-        assertEquals("Valoración Competencias Ser Josias Montoya.xlsx",fileName.toString());
+        assertEquals("Valoración Competencias Ser Josias Montoya.xlsx", fileName.toString());
     }
 
     @Test
     public void getFileNameWithOneDates() throws Exception {
-        StringBuilder fileName = fileService.getFileName("Josias Montoya", null, "2017-03-14");
+        StringBuilder fileName = fileService.getFileName("Josias Montoya",
+                null, "2017-03-14");
 
-        assertEquals("Valoración Competencias Ser Josias Montoya al 2017-03-14.xlsx",fileName.toString());
+        assertEquals("Valoración Competencias Ser Josias Montoya al 2017-03-14.xlsx", fileName.toString());
     }
 
 }
