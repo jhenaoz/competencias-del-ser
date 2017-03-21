bf8c6329836b4f641293f43d8ae8f5d1e5d94826
salveara <santiago.aalvear@gmail.com>
2017-03-21 14:07:09 -0500

    change password

---
+++ src/main/java/co/com/psl/evaluacionser/service/dto/Administrator.java
@@ -0,0 +1,39 @@
+package co.com.psl.evaluacionser.service.dto;
+
+public class Administrator {
+
+    private String username;
+    private String password;
+    private String token;
+    private String timestamp;
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getTimeStamp() {
+        return timestamp;
+    }
+
+    public void setTimeStamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
