84aa72afa01fb9b6620d762e9edf50222152c479
salveara <santiago.aalvear@gmail.com>
2017-03-29 11:24:41 -0500

    import deleted

--- src/main/java/co/com/psl/evaluacionser/controller/PasswordController.java
+++ src/main/java/co/com/psl/evaluacionser/controller/PasswordController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
